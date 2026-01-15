d034dd1d4b93f75b06a2893fc066471dda2f873e
VicThor <vvegag21@gmail.com>
2026-01-15 23:19:24 +0100

    implementado el controlador sin servicio implementado del formulario perro

---
+++ src/main/java/com/proyecto/spring/protectoracanina/controller/FormPerroController.java
@@ -0,0 +1,22 @@
+package com.proyecto.spring.protectoracanina.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/formperro")
+public class FormPerroController {
+	
+	@GetMapping
+	public String formularioPerro(Model model) {
+		log.info("[formularioPerro]");
+		return "/formperro";
+	}
+	
+
+}
